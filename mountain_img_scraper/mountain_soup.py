9ae3fa73a93269225c79fd94d97599b3c0b99d4d
geek-on-a-bike <geekonabike2018@gmail.com>
2022-10-26 13:41:28 -0700

    Did some more work and testing with Selenium, so far script is able to open chrome, search for search phrase on google, and open its image page.

--- mountain_img_scraper/mountain_soup.py
+++ mountain_img_scraper/mountain_soup.py
@@ -10,8 +10,15 @@ NOTE: this is intended for use with a virtualenv, which can be activated
 by switching to the "mountainenv" directory and typing into terminal:
 source bin/activate
 
-- by Alder French, with help from this tutorial:
+NOTE 2: this program requires having Selenium and Chromedriver setup, with 
+chromedriver added to PATH.
+
+- by Alder French, with help from this tutorial (beautifulsoup):
 https://www.makeuseof.com/python-scrape-web-images-how-to/
+and this tutorial (selenium):
+https://www.scrapingbee.com/blog/selenium-python/
+and this tutorial (scraping from google images):
+https://medium.com/geekculture/scraping-images-using-selenium-f35fab26b122
 
 USAGE:
 Given a .txt list of search phrases, one phrase per line, in terminal type:
@@ -19,24 +26,48 @@ Given a .txt list of search phrases, one phrase per line, in terminal type:
 
 """
 
+from logging import exception
 import requests
+import time
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import TimeoutException
+
+
 
 
 
 #SELENIUM PART OF PROJECT WORK IN PROGRESS FROM HERE
 """
 CONSTANTS    """
-DRIVER_PATH = '~/Desktop/RoboFarmer3000/chromedriver'
-GOOG_IMG_BTN_CLASS_CODE = 'cDaxAd'
+#DRIVER_PATH = "/home/Alder French/Desktop/RoboFarmer3000/chromedriver"
+# added driver path to BASH so we don't need the above
+GOOG_IMG_BTN_CLASS_CODE = "cDaxAd"
+SEARCH_PHRASES = ["mt hood", "Rainier", "mt st helens"] #for testing!
+GOOGLE_SEARCH_STARTER = "https://www.google.com/search?q="
 
+""" SELENIUM TEST - (WORKS)
 options = Options()
 options.headless = True
 options.add_argument("--window-size=1920,1200")
 
-driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
+service = Service(DRIVER_PATH)
+#service.start()
+#driver = webdriver.Remote(service.service_url)
+#driver = webdriver.Chrome(DRIVER_PATH, options=options)
+driver = webdriver.Chrome() #THIS WORKS! YAY GOT SELENIUM WORKING!
+
+
+driver.get("https://www.nintendo.com/")
+time.sleep(5)
+print(driver.page_source)
+driver.quit() END TEST"""
 
 '''GET SEARCH PHRASES FROM .TXT LIST, THEN LOOP THROUGH EACH ONE DOING:
 1. Get the google search page for that phrase
@@ -64,8 +95,40 @@ and gracefully exit the search for that term.
 6. Repeat steps 1 - 6 for each search phrase by using the for loop. 
 
 '''
-for search in search_phrases:
-    driver.get('https://google.com')
+search_phrases = ["mt hood"] #SEARCH_PHRASES
+driver = webdriver.Chrome()  # THIS WORKS! YAY GOT SELENIUM WORKING!
+for phrase in search_phrases:
+    words = phrase.split()
+    if len(words) <= 0:
+        print("WARNING: The length of this phrase is <= 0: " + phrase)
+        continue
+    end_of_search = words[0]
+    for word in words[1:]:
+        end_of_search  = end_of_search + "+" + word
+    full_search = GOOGLE_SEARCH_STARTER + end_of_search
+    print("Test: full_search is: " + full_search)
+    # ABOVE CREATES URL TO SEARCH IN GOOGLE
+    # NEXT SEARCH FOR THAT URL AND WAIT FOR IMAGE BUTTON
+    driver.get(full_search)
+    try:
+        goog_img_btn = WebDriverWait(driver, 7).until(
+            #EC.element_to_be_clickable((By.CLASS_NAME, "jmeM8b"))
+            EC.presence_of_element_located(
+                (By.XPATH, "/html/body/div[7]/div/div[4]/div/div[1]/div/div[1]/div/div[2]/a"))
+        ) #XPATH WORKS, By. CLASS_NAME DOESN'T!!!
+    except NoSuchElementException:
+        print("Encountered a NoSuchElement expection in phrase for loop: " + str(NoSuchElementException))
+    except TimeoutException:
+        print("Encountered a timeout expection in phrase for loop: " +
+              str(TimeoutException))
+    else:
+        print("OPENING THE GOOGLE IMAGES PAGE FOR: " + phrase)
+        goog_img_btn.click()
+        time.sleep(7) 
+    finally:
+        print("Made it to finally block")
+        #driver.quit()
+
 
 
 
@@ -73,10 +136,11 @@ for search in search_phrases:
  #TO HERE END SELENIUM WORK IN PROGRESS
 
 
-"""NOTE: What I have below is a good start especially for resolving
+""" BEGIN BeautifulSoup TEST (WORKS)
+NOTE: What I have below is a good start especially for resolving
 weird URLs and downloading images from URLs to folders, but I will need to
 install and use SELENIUM to scrape data without using an expensive API.
-"""
+
 
 URL = "https://www.google.com/search?q=grand+tetons&tbm=isch&sxsrf=ALiCzsYGUyL81tfzBDq7Hgjxd4bTE_MdHQ%3A1666680807354&source=hp&biw=1280&bih=603&ei=54dXY9CaE8yD0PEP__WLYA&iflsig=AJiK0e8AAAAAY1eV9wFlyi6QfUQr22Lg_wD3pe-Omr3T&ved=0ahUKEwiQquXr5fr6AhXMATQIHf_6AgwQ4dUDCAY&uact=5&oq=grand+tetons&gs_lcp=CgNpbWcQAzIECCMQJzIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDoHCCMQ6gIQJzoICAAQsQMQgwE6CAgAEIAEELEDOgsIABCABBCxAxCDAVC6FFjyLGC6LmgDcAB4AIABZIgBvAeSAQQxMy4xmAEAoAEBqgELZ3dzLXdpei1pbWewAQo&sclient=img#imgrc=Z51xKzTA1MX3sM" #Paste URL here
 
@@ -98,4 +162,5 @@ for image in resolvedURLs:
 print("SUCCESSFULLY SCRAPED PHOTOS!")
 #STOPPED HERE 10/25/2022 TESTED AND WORKING SO FAR!
 
+END BEAUTIFUL SOUP TEST"""
 
